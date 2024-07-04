20e1970ca0b6d42bd375e4a131b4e79cebe99777
Kavyan Nayak <kavyannayak104@gmail.com>
2024-07-04 11:25:37 +0530

    New

--- Algo LAB/2_PaperQ1.c
+++ Algo LAB/2_PaperQ1.c
@@ -1,46 +1,39 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void selectionshort(int arr[], int n, int *comparison_count)
-{
-    int i,j,min_index, temp;
+void selectionSort(int arr[], int n, int *comparison_count) {
+    int i, j, min_index, temp;
     *comparison_count = 0;
-
-    for (i = 0; i < n-1; i++)
-    {
+    
+    for (i = 0; i < n-1; i++) {
+        
         min_index = i;
-
-        for (j = 0; j < n; j++)
-        {
+        for (j = i + 1; j < n; j++) {
             (*comparison_count)++;
-            if (arr[j] < arr[min_index])
-            {
+            if (arr[j] < arr[min_index]) {
                 min_index = j;
             }
         }
+        
         temp = arr[min_index];
         arr[min_index] = arr[i];
         arr[i] = temp;
     }
 }
 
-int main()
-{
-    int arr[] = {17,7,8,15,6,3};
-
+int main() {
+    int arr[] = {17, 7, 8, 15, 6, 3};
     int n = sizeof(arr) / sizeof(arr[0]);
-
-    int comoarision_count;
-
-    selectionshort(arr, n, &comoarision_count);
-
-    printf("Sorted array :");
-    for (int i = 0; i < n; i++)
-    {
-        printf("%d", arr[i]);
+    int comparison_count;
+    
+    selectionSort(arr, n, &comparison_count);
+    
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
     }
     printf("\n");
-
-    printf("Total number of comparisons: %d\n", comoarision_count);
+    
+    printf("Total number of comparisons: %d\n", comparison_count);
     
     return 0;
 }
