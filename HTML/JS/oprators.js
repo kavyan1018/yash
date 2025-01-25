c5a00b0879f9c7fc9a073633f94a90272de27868
Kavyan Nayak <kavyannayak104@gmail.com>
2025-01-25 10:48:21 +0530

    Oprators

---
+++ HTML/JS/oprators.js
@@ -0,0 +1,66 @@
+
+    // oprators in js
+    // 1. Arithmatic operators
+    // 2. Assignment operators
+    // 3. Comparison operators
+    // 4. Logical operators
+    // 5. Bitwise operators
+    // 6. Conditional operators
+    
+    // 1. Arithmatic operators
+    // +, -, *, /, %, ++, --
+
+    // var a = 20;
+    // var b = 20;
+    // document.write(a+b);
+
+    // 2. Assignment operators
+    // =, +=, -=, *=, /=, %=
+
+    // var a = 50;
+    // var b = 20;
+    // a += b;
+    // document.write(a);
+
+    // 3. Comparison operators
+    // ==, ===, !=, !==, >, <, >=, <=
+
+    // var a = 30;
+    // var b = 20;
+
+    // if(a > b){
+    //     document.write("a is greater than b");
+    // }else{
+    //     document.write("b is greater than a");
+    // }
+
+    // 4. Logical operators
+    // &&, ||, !
+
+    // var a = 5;
+    // var b = 20;
+
+    // if(a > b && a > 10){
+    //     document.write("a is greater than b and a is greater than 10");
+    // }else{
+    //     document.write("b is greater than a");
+    // }
+
+    // 5. Bitwise operators
+    // &, |, ^, ~, <<, >>
+    
+    // var a = 50;
+    // var b = 20;
+
+    // document.write(a & b);
+
+    // 6. Conditional operators
+    // ?:, typeof, instanceof   
+
+    // var a = 50;
+    // var b = 20;
+
+    // var result = (a > b) ? "a is greater than b" : "b is greater than a";
+
+    // document.write(result);
+
